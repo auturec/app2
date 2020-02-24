1d859e7931c9cb3b274db28bb516c52045b4eb7e
nwjbrandon <nwjbrandon@gmail.com>
2020-02-25 00:30:12 +0800

    refactor onboarding page

--- src/routes/Onboarding/Slide.jsx
+++ src/routes/Onboarding/Slide.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import CharacterSlide1 from '../../assets/Onboarding/CharacterSlide1.svg';
 
 const useStyles = makeStyles(() => ({
   characterSlide: {
     height: '300px',
     margin: '0 auto',
     marginTop: '30px',
+  },
+  header: {
+    color: 'teal',
+    fontSize: '30px',
+    marginLeft: '10px',
+    marginTop: '30px',
+    fontWeight: 'bold',
+  },
+  subheader: {
+    fontSize: '20px',
+    marginLeft: '10px',
+    marginTop: '10px',
+    fontWeight: 'bold',
   }
 }));
 
-function Slide({ header, subheader }) {
+const Slide = ({ img, header, subheader }) => {
   const classes = useStyles();
 
   return (
     <div>
       <Grid container>
         <Grid item xs={12} sm={6}>
-          <img src={CharacterSlide1} alt="" className={classes.characterSlide} />
+          <img src={img} alt="" className={classes.characterSlide} />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <div>{header}</div>
-          <div>{subheader}</div>
+          <div className={classes.header}>{header}</div>
+          <div className={classes.subheader}>{subheader}</div>
         </Grid>
       </Grid>
     </div>
