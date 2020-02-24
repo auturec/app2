a25b536825f57ec059fb7485759481a8fe74d1c3
nwjbrandon <nwjbrandon@gmail.com>
2020-02-25 00:46:59 +0800

    refactor signin page

--- src/routes/Signin/Signin.jsx
+++ src/routes/Signin/Signin.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
+import {
+  FORGOT_PASSWORD,
+  SIGNUP,
+} from '../../constants/routes';
+import FullLogo from '../../assets/Logo/Full_Logo.svg';
+
+const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
@@ -33,9 +37,9 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  fullLogo: {
+    margin: '0px auto',
+    width: '100px',
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -44,18 +48,19 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  subtext: {
+    color: '#3f51b5',
+  }
 }));
 
-export default function Signin() {
+const Signin = () => {
   const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <img src={FullLogo} alt="" className={classes.fullLogo}/>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
@@ -97,12 +102,12 @@ export default function Signin() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link to={FORGOT_PASSWORD} variant="body2" className={classes.subtext}>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to={SIGNUP} variant="body2" className={classes.subtext}>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
@@ -115,3 +120,5 @@ export default function Signin() {
     </Container>
   );
 }
+
+export default Signin;
