45d34a4e53a181a14c1527f0e2c7a84909664344
Hanming Zhu <hanming@huawen.com>
2020-03-01 02:16:46 +0800

    feat: add providers into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import AppProviders from "./contexts";
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.scss";
+
+const AppRender = () => {
+  return (
+    <AppProviders>
+      {/* <BrowserRouter> */}
+      <App />
+      {/* </BrowserRouter> */}
+    </AppProviders>
+  );
+};
+
+ReactDOM.render(<AppRender />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
