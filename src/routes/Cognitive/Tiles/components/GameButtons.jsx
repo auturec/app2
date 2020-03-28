074ec8cf9a9f898c55b0bc5075d71dcc9e4f9bef
BobbyZhouZijian <zephyroszhou@gmail.com>
2020-03-28 19:32:31 +0800

    switch buttons from bulma to material ui

--- src/routes/Cognitive/Tiles/components/GameButtons.jsx
+++ src/routes/Cognitive/Tiles/components/GameButtons.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import { useToasts } from 'react-toast-notifications';
 
+import { ButtonGroup } from '@material-ui/core';
 import { TilesImageMap } from '../TilesImageMap';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  redbutton: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  },
+
+  greenbutton: {
+    background: 'linear-gradient(to right, #78ffd6, #a8ff78)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  },
+
+  yellowbutton: {
+    background: 'linear-gradient(to right, #F2C94C, #F2994A)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  }
+});
 
 const Gamepapers = props => {
   const {
@@ -64,29 +105,33 @@ const Gamepapers = props => {
     handleClearAll(e);
   };
 
+  const classes = useStyles();
+
   return (
-    <div className="buttons has-addons is-centered">
-      <button
-        className="button is-dark has-text-white"
-        onClick={e => handleAnswer(e)}
-        type="button"
-      >
-        Check Answer
-      </button>
-      <button
-        className="button is-info has-text-black"
-        onClick={e => handleReset(e)}
-        type="button"
-      >
-        Reset Game
-      </button>
-      <button
-        className="button is-warning has-text-black"
-        onClick={e => handleClearAll(e)}
-        type="button"
-      >
-        Clear All Selections
-      </button>
+    <div className={classes.root}>
+      <ButtonGroup aria-label="outlined secondary button group">
+        <Button
+          className={classes.yellowbutton}
+          onClick={e => handleAnswer(e)}
+          type="button"
+        >
+          Check Answer
+        </Button>
+        <Button
+          className={classes.greenbutton}
+          onClick={e => handleReset(e)}
+          type="button"
+        >
+          Reset Game
+        </Button>
+        <Button
+          className={classes.redbutton}
+          onClick={e => handleClearAll(e)}
+          type="button"
+        >
+          Clear Selections
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
