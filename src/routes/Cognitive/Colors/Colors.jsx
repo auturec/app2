5a5453fc853a7b8ed9443f2cac6bdc3ec8ee3818
nwjbrandon <nwjbrandon@gmail.com>
2020-05-15 22:48:40 +0800

    add random feature

--- src/routes/Cognitive/Colors/Colors.jsx
+++ src/routes/Cognitive/Colors/Colors.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Container, makeStyles } from '@material-ui/core';
+import {
+  shuffleArray,
+  getNRandomElements,
+  getRandomElement,
+} from 'utils/randomUtils';
 import './Colors.scss';
 
 const useStyles = makeStyles((theme) => ({
@@ -37,9 +42,34 @@ class ClickMe extends React.Component {
 
 const TapColors = () => {
   const classes = useStyles();
+  const [colors, handleColors] = React.useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+  ]);
+
+  const getRandomTwoColors = () => {
+    handleColors(shuffleArray(colors));
+    const c = getNRandomElements(colors, 2);
+    const a = getRandomElement(c);
+    console.log(c);
+    console.log(a);
+  };
+
   return (
     <Container component="main" maxWidth="sm">
       <div className={classes.paper}>
+        <div>TOUCH RED</div>
+        <button type="button" onClick={getRandomTwoColors}>
+          Click me
+        </button>
         <div className={classes.box}>This is the colors page</div>
         <ClickMe />
       </div>
