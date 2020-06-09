dbe005c3785da75808a3e93558fdd3602a6f3c0b
namiwa <khairul.nasri895@gmail.com>
2020-06-09 17:00:18 +0800

    associate: began refactoring

--- src/routes/Cognitive/Associate/AssociateGame.jsx
+++ src/routes/Cognitive/Associate/AssociateGame.jsx
@@ -2,35 +2,14 @@ import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { useToasts } from 'react-toast-notifications';
 
+import {
+  getRandomElement,
+  getNRandomElements,
+  shuffleArray,
+} from 'utils/randomUtils';
 import { ImageMap, ImageKeyList } from './GameImages';
 import MainImage from './MainImage';
 import MultipleOptions from './MultipleOptions';
-
-/**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- * referencing https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
- * from https://stackoverflow.com/users/310500/laurens-holst
- */
-const shuffleArray = (original) => {
-  const array = original.slice();
-  for (let i = array.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
-
-const getRandomFromArray = (array) => {
-  const ranId = Math.floor(array.length * Math.random());
-  return array[ranId];
-};
-
-const getFourList = (array) => {
-  const copy = array.slice();
-  const shuffled = shuffleArray(copy);
-  return shuffled.slice(0, 4);
-};
 
 const getRandomPairFromArray = (array) => {
   const randomArray = shuffleArray(array);
@@ -51,10 +30,10 @@ const getRandomPairFromArray = (array) => {
 const AssociateGame = () => {
   const { addToast } = useToasts();
   const [allTheme] = useState({
-    themes: getFourList(ImageKeyList),
+    themes: getNRandomElements(ImageKeyList, 4),
   });
   const [currentTheme] = useState({
-    theme: getRandomFromArray(allTheme.themes),
+    theme: getRandomElement(allTheme.themes),
   });
   const [answerPair] = useState({
     pair: getRandomPairFromArray(ImageMap[currentTheme.theme]),
@@ -65,7 +44,7 @@ const AssociateGame = () => {
       if (val === answerTheme) {
         return { image: answerPair.pair.second, theme: answerTheme };
       }
-      return { image: getRandomFromArray(ImageMap[val]), theme: val };
+      return { image: getRandomElement(ImageMap[val]), theme: val };
     });
   };
 
