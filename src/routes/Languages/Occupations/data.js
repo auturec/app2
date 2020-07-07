76de085e528e2f58717f31d20feb95aa38f35f69
Hanming Zhu <hanming@huawen.com>
2020-07-07 12:13:19 +0800

    feat: add other difficulty images to occupationsData

--- src/routes/Languages/Occupations/data.js
+++ src/routes/Languages/Occupations/data.js
@@ -1,97 +1,135 @@
 import React from 'react';
 
-import { MEDIUM } from 'constants/difficulties';
-import * as OccupationMediumSvg from 'assets/images/occupations/medium';
+import { EASY, MEDIUM, HARD } from 'constants/difficulties';
+import * as OccupationEasy from 'assets/images/occupations/easy';
+import * as OccupationMedium from 'assets/images/occupations/medium';
+import * as OccupationHard from 'assets/images/occupations/hard';
 import { occupationSounds } from './sounds';
 
 export const occupationData = [
   {
-    [MEDIUM]: <OccupationMediumSvg.Astronaut style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Astronaut style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Astronaut style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Astronaut style={{ height: 'auto' }} />,
     name: 'Astronaut',
     ...occupationSounds.astronaut,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Baker style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Baker style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Baker style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Baker style={{ height: 'auto' }} />,
     name: 'Baker',
     ...occupationSounds.baker,
   },
+  // {
+  //   [EASY]: <OccupationEasy.Barber style={{ height: 'auto' }} />,
+  //   [MEDIUM]: <OccupationMedium.Barber style={{ height: 'auto' }} />,
+  //   [HARD]: <OccupationHard.Astronaut style={{ height: 'auto' }} />,
+  //   name: 'Barber',
+  //   ...occupationSounds.barber,
+  // },
+  // {
+  //   [EASY]: <OccupationEasy.Cashier style={{ height: 'auto' }} />,
+  //   [MEDIUM]: <OccupationMedium.Cashier style={{ height: 'auto' }} />,
+  //   [HARD]: <OccupationHard.Astronaut style={{ height: 'auto' }} />,
+  //   name: 'Cashier',
+  //   ...occupationSounds.cashier,
+  // },
   {
-    [MEDIUM]: <OccupationMediumSvg.Barber style={{ height: 'auto' }} />,
-    name: 'Barber',
-    ...occupationSounds.barber,
-  },
-  {
-    [MEDIUM]: <OccupationMediumSvg.Cashier style={{ height: 'auto' }} />,
-    name: 'Cashier',
-    ...occupationSounds.cashier,
-  },
-  {
-    [MEDIUM]: <OccupationMediumSvg.Chef style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Chef style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Chef style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Chef style={{ height: 'auto' }} />,
     name: 'Chef',
     ...occupationSounds.chef,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Doctor style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Doctor style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Doctor style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Doctor style={{ height: 'auto' }} />,
     name: 'Doctor',
     ...occupationSounds.doctor,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Farmer style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Farmer style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Farmer style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Farmer style={{ height: 'auto' }} />,
     name: 'Farmer',
     ...occupationSounds.farmer,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Fireman style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Fireman style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Fireman style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Fireman style={{ height: 'auto' }} />,
     name: 'Fireman',
     ...occupationSounds.fireman,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Librarian style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Librarian style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Librarian style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Librarian style={{ height: 'auto' }} />,
     name: 'Librarian',
     ...occupationSounds.librarian,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Nurse style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Nurse style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Nurse style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Nurse style={{ height: 'auto' }} />,
     name: 'Nurse',
     ...occupationSounds.nurse,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Painter style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Painter style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Painter style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Painter style={{ height: 'auto' }} />,
     name: 'Painter',
     ...occupationSounds.painter,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Pilot style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Pilot style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Pilot style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Pilot style={{ height: 'auto' }} />,
     name: 'Pilot',
     ...occupationSounds.pilot,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Policeman style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Policeman style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Policeman style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Policeman style={{ height: 'auto' }} />,
     name: 'Policeman',
     ...occupationSounds.policeman,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Scientist style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Scientist style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Scientist style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Scientist style={{ height: 'auto' }} />,
     name: 'Scientist',
     ...occupationSounds.scientist,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Student style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Student style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Student style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Student style={{ height: 'auto' }} />,
     name: 'Student',
     ...occupationSounds.student,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Teacher style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Teacher style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Teacher style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Teacher style={{ height: 'auto' }} />,
     name: 'Teacher',
     ...occupationSounds.teacher,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Vet style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Vet style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Vet style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Vet style={{ height: 'auto' }} />,
     name: 'Vet',
     ...occupationSounds.vet,
   },
   {
-    [MEDIUM]: <OccupationMediumSvg.Waiter style={{ height: 'auto' }} />,
+    [EASY]: <OccupationEasy.Waiter style={{ height: 'auto' }} />,
+    [MEDIUM]: <OccupationMedium.Waiter style={{ height: 'auto' }} />,
+    [HARD]: <OccupationHard.Waiter style={{ height: 'auto' }} />,
     name: 'Waiter',
     ...occupationSounds.waiter,
   },
