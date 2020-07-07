e31a8a09b1c9228b07c20dbeab09712464d54c8f
Hanming Zhu <hanming@huawen.com>
2020-07-07 12:12:32 +0800

    feat: update hard occupation images with a jsx file each

---
+++ src/assets/images/occupations/hard/Chef.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ChefJpg from './Chef.jpg';
+
+const Chef = ({ style }) => {
+  return <img src={ChefJpg} style={style} alt="Chef" />;
+};
+
+export default Chef;
