761620824da22cbcea116e8baea87aaab2bfd181
Hanming Zhu <hanming@huawen.com>
2020-07-07 12:13:40 +0800

    feat: add logic for difficulty selection in optionsSelector

--- src/components/GameTemplate/OptionsSelector.jsx
+++ src/components/GameTemplate/OptionsSelector.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Button } from '@material-ui/core';
 
-import { MEDIUM } from 'constants/difficulties';
+import { EASY, MEDIUM, HARD } from 'constants/difficulties';
 import { useGame } from 'contexts/GameContext';
 import GameTemplate from './GameTemplate';
 
@@ -9,31 +9,44 @@ import './GameTemplate.scss';
 
 const OptionsSelector = ({ allOptions }) => {
   const [numberOfOptionsPerRound, setNumberOfOptionsPerRound] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+  const [startGame, setStartGame] = useState(false);
   const { isResettingGame, setIsResettingGame } = useGame();
 
   useEffect(() => {
     if (isResettingGame) {
+      setStartGame(false);
       setNumberOfOptionsPerRound(null);
+      setDifficulty(null);
       setIsResettingGame(false);
     }
   }, [isResettingGame, setIsResettingGame]);
 
-  if (numberOfOptionsPerRound) {
+  if (startGame && numberOfOptionsPerRound && difficulty) {
     return (
       <GameTemplate
         allOptions={allOptions}
         numberOfOptionsPerRound={numberOfOptionsPerRound}
-        difficulty={MEDIUM}
+        difficulty={difficulty}
       />
     );
   }
+
+  const difficulties = [];
+
+  if (allOptions.every((o) => Object.prototype.hasOwnProperty.call(o, EASY)))
+    difficulties.push(EASY);
+  if (allOptions.every((o) => Object.prototype.hasOwnProperty.call(o, MEDIUM)))
+    difficulties.push(MEDIUM);
+  if (allOptions.every((o) => Object.prototype.hasOwnProperty.call(o, HARD)))
+    difficulties.push(HARD);
 
   return (
     <Container component="main" maxWidth="sm">
       <div className="options-selector">
         <h2>Please select the number of options presented in each round:</h2>
         <Button
-          color="primary"
+          color={numberOfOptionsPerRound === 2 ? 'primary' : 'default'}
           variant="contained"
           size="large"
           fullWidth
@@ -42,7 +55,7 @@ const OptionsSelector = ({ allOptions }) => {
           TWO
         </Button>
         <Button
-          color="primary"
+          color={numberOfOptionsPerRound === 3 ? 'primary' : 'default'}
           variant="contained"
           size="large"
           fullWidth
@@ -51,13 +64,52 @@ const OptionsSelector = ({ allOptions }) => {
           THREE
         </Button>
         <Button
-          color="primary"
+          color={numberOfOptionsPerRound === 4 ? 'primary' : 'default'}
           variant="contained"
           size="large"
           fullWidth
           onClick={() => setNumberOfOptionsPerRound(4)}
         >
           FOUR
+        </Button>
+
+        {difficulties.length > 1 && (
+          <>
+            <h2 className="difficulty-title">
+              Please select the difficulty level:
+            </h2>
+            {difficulties.map((d) => (
+              <Button
+                color={difficulty === d ? 'primary' : 'default'}
+                variant="contained"
+                size="large"
+                fullWidth
+                onClick={() => setDifficulty(d)}
+                key={d}
+              >
+                {d.toUpperCase()}
+              </Button>
+            ))}
+          </>
+        )}
+
+        <Button
+          color="primary"
+          variant="contained"
+          size="large"
+          fullWidth
+          className="start-game-button"
+          onClick={() => {
+            if (difficulties.length === 1) {
+              setDifficulty(difficulties[0]);
+            }
+            setStartGame(true);
+          }}
+          disabled={
+            !numberOfOptionsPerRound || (difficulties.length > 1 && !difficulty)
+          }
+        >
+          <strong>START GAME</strong>
         </Button>
       </div>
     </Container>
